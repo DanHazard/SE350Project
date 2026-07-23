759a5bb65d76e06f31ff9f8a8ed67ab641ff4639
DanHazard <daniel.love.jr@gmail.com>
2026-07-23 14:09:49 -0400

    basic test that works somehow.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,4 +2,15 @@ package main;
 
 public class Main {
     // Need to test Match.
+
+    public static void main(String[] args) {
+
+        // Testing to see if the logic is wired up correctly.
+
+        Player dan = new Player("Dan");
+        Player anna = new Player("Anna");
+        Match match = new Match(dan, anna);
+        match.render(4);
+
+    }
 }
